0a29d886e284af25c74b4f8cae4722905ac085c7
uschuster <uschuster@b991c9e4-d40e-a444-a52a-6eeef943019f>
2009-03-21 13:47:17 +0000

    bulk replace of "Subversion plugin for Borland Delphi" with "...CodeGear Delphi" to match the project name at SF

--- src/svn_client.pas
+++ src/svn_client.pas
@@ -1,6 +1,6 @@
 {**********************************************************************************************************************}
 {                                                                                                                      }
-{ delphisvn: Subversion plugin for Borland Delphi                                                                      }
+{ delphisvn: Subversion plugin for CodeGear Delphi                                                                     }
 {                                                                                                                      }
 { The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); you may not use     }
 { this file except in compliance with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/ }
